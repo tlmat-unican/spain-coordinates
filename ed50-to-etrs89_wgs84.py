ca616ecec8a7fe10fe7532bb3d98b0b12ff5e4b9
Jorge Lanza <jlanza@tlmat.unican.es>
2018-07-18 14:36:50 +0200

    Enhanced with dict

--- ed50-to-etrs89_wgs84.py
+++ ed50-to-etrs89_wgs84.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 
 # Define the projections using EPSG codes, check spatialreference.org for help
 # Add the shift grid with nadgrids option
-ZONES=["29N", "30N", "31N"]
-ED50_UTM29N = pyproj.Proj(init="EPSG:23029", nadgrids="./ntv2_grids/PENR2009.gsb")
-ED50_UTM30N = pyproj.Proj(init="EPSG:23030", nadgrids="./ntv2_grids/PENR2009.gsb")
-ED50_UTM31N = pyproj.Proj(init="EPSG:23031", nadgrids="./ntv2_grids/PENR2009.gsb,./ntv2_grids/BALR2009.gsb ")
+ZONES = {
+    "29N": pyproj.Proj(init="EPSG:23029", nadgrids="./ntv2_grids/PENR2009.gsb"),
+    "30N": pyproj.Proj(init="EPSG:23030", nadgrids="./ntv2_grids/PENR2009.gsb"),
+    "31N": pyproj.Proj(init="EPSG:23031", nadgrids="./ntv2_grids/PENR2009.gsb,./ntv2_grids/BALR2009.gsb ")
+}
+
 WGS84 = pyproj.Proj(init="epsg:4326")
 
 class InvalidUsage(Exception):
@@ -37,21 +39,22 @@ def handle_invalid_usage(error):
 def transform(zone):
     if zone not in ZONES:
         raise InvalidUsage('Not valid zone for Spain.', status_code=404)
+    
     if request.method == 'GET':
-        return do_get()
+        return do_get(ZONES[zone])
     else:
-        return do_post()
+        return do_post(ZONES[zone])
 
-def do_get():
+def do_get(ED50_zone):
     ED50_x = request.args.get('x')
     ED50_y = request.args.get('y')
     if ED50_x is None or ED50_x == '' or ED50_y is None or ED50_y == '':
         raise InvalidUsage('Must set both x and y query params.', status_code=400)
     else:
-        WGS84_long, WGS84_lat = pyproj.transform(ED50_UTM30N, WGS84, ED50_x, ED50_y)
+        WGS84_long, WGS84_lat = pyproj.transform(ED50_zone, WGS84, ED50_x, ED50_y)
         return jsonify(WGS84_long, WGS84_lat)
 
-def do_post():
+def do_post(ED50_zone):
     if request.content_type != 'application/json':
         raise InvalidUsage('Only accepts application/json.', status_code=415)
     try:
@@ -60,7 +63,7 @@ def do_post():
         for coordinates in json_data:
             ED50_x = coordinates[0]
             ED50_y = coordinates[1]
-            WGS84_long, WGS84_lat = pyproj.transform(ED50_UTM30N, WGS84, ED50_x, ED50_y)
+            WGS84_long, WGS84_lat = pyproj.transform(ED50_zone, WGS84, ED50_x, ED50_y)
             output.append([WGS84_long, WGS84_lat])
     except:
         raise InvalidUsage("Invalid JSON format. Include array of coordinates (i.e. [[x1,y1],...,[xn,yn]].", status_code=400)
@@ -71,13 +74,9 @@ if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
 
 
-# # Do the transformation:
-# ED50_x = 433829.9531810064
-# ED50_y = 4811755.325688820
-# ETRS89_x, ETRS89_y = pyproj.transform(ED50_UTM30N, WGS84, ED50_x, ED50_y)
-
-# print ("ETRS89 UTM zone 30 coordinates = (%0.8f, %0.8f)" % (ETRS89_x, ETRS89_y))
-#ETRS89 UTM zone 30 coordinates = (3,81917591, 43,45391861)
+# Example transformation:
+# ED50 UTM zone 30 coordinates = (433829.9531810064, 4811755.325688820)
+# ETRS89 UTM zone 30 coordinates = (3,81917591, 43,45391861)
 
 
 
